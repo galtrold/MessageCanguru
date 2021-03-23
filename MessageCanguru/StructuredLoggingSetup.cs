c08a975570bb370bc36c1e95d35be1640148d9de
Christian James <cjh@pentia.dk>
2021-03-23 08:33:10 +0100

    Initial commit

---
+++ MessageCanguru/StructuredLoggingSetup.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+using Sitecore.Pipelines;
+
+namespace MessageCanguru
+{
+    public class StructuredLoggingSetup
+    {
+        public void Process(PipelineArgs args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+        }
+    }
+}
